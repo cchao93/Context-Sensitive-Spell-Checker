ce332f5ff71e2306a23e01ca013c992eaf56a60e
Victor Chao <chia-chi.chao@tufts.edu>
2015-12-21 06:17:54 +0800

    failed viterbi attempt

--- spell-checker.py
+++ spell-checker.py
@@ -7,6 +7,9 @@ unknown_token = "<UNK>"
 start_token = "<S>"
 end_token = "</S>"
 
+def TreebankNoTraces():
+    return [[x for x in sent if x[1] != "-NONE-"] for sent in treebank.tagged_sents()]
+
 def BuildVocabulary(corpus):
 	vocabulary = defaultdict(int)
 	for sent in corpus:
@@ -38,89 +41,6 @@ def UntagCorpus(corpus):
 		untagged_corpus.append(untagged_sent)
 	return untagged_corpus
 
-class TrigramHMM:
-    def __init__(self):
-        self.transitions = defaultdict(float)
-        self.emissions = defaultdict(float)
-        self.dictionary = defaultdict(lambda:list())
-
-    def EstimateTransitions(self, training_set):
-        bigram_counts = defaultdict(int)
-        for sent in training_set:
-            prev = 1
-            prev2 = 0
-            for curr in xrange(2, len(sent)):
-                trigram = (sent[prev2][1], sent[prev][1], sent[curr][1])
-                self.transitions[trigram] += 1
-                bigram = (sent[prev2][1], sent[prev][1])
-                bigram_counts[bigram] += 1
-                prev += 1
-                prev2 += 1
-        for trigram, freq in self.transitions.iteritems():
-            bigram = (trigram[0], trigram[1])
-            self.transitions[trigram] = (float)(freq) / (float)(bigram_counts[bigram])
-
-    def EstimateEmissions(self, training_set):
-        unigram_counts = defaultdict(int)
-        for sent in training_set:
-            for bigram in sent:
-                unigram_counts[bigram[1]] += 1
-                self.emissions[bigram] += 1
-        for bigram, freq in self.emissions.iteritems():
-            self.emissions[bigram] = (float)(freq) / (float)(unigram_counts[bigram[1]])
-
-    def ComputeTagDictionary(self, training_set):
-        for sent in training_set:
-            for word in sent:
-                if word[1] not in self.dictionary[word[0]]:
-                    self.dictionary[word[0]].append(word[1])
-
-    def Train(self, training_set):
-    	self.EstimateTransitions(training_set)
-    	self.EstimateEmissions(training_set)
-    	self.ComputeTagDictionary(training_set)
-
-    def Viterbi(self, sent):
-        T = len(sent)
-        trellis = [{} for i in range(T)]
-        backptr = [{} for i in range(T)]
-
-        trellis[0][start_token] = 1.0
-        backptr[0][start_token] = None
-
-        for i in range(1, T):
-            word = sent[i]
-            tags = self.dictionary[word]
-            assert(tags)
-            for tag in tags:
-                max_path_prob = None
-                max_path_prev_tag = None
-                for prev_tag, prev_path_prob in trellis[i - 1].iteritems():
-                    path_prob = prev_path_prob * self.transitions[(prev_tag, tag)] * self.emissions[(word, tag)]
-                    if path_prob > max_path_prob and (max_path_prob or path_prob != float("-inf")):
-                        max_path_prob = path_prob
-                        max_path_prev_tag = prev_tag
-                trellis[i][tag] = max_path_prob
-                backptr[i][tag] = max_path_prev_tag
-        best_path_prob = trellis[T - 1][end_token]
-        best_path = self.Backtrace(sent, backptr)
-        return best_path
-
-    def Backtrace(self, sent, backptr, index = None, tag = end_token):
-        if index == 0:
-        	return [tag]
-        if index == None:
-        	index = len(sent) - 1
-        prev_tag = backptr[index][tag]
-        prev_best_path = self.Backtrace(sent, backptr, index - 1, prev_tag)
-        return prev_best_path + [tag]
-
-    def Test(self, test_set):
-        predicted_test_set = list()
-        for sent in test_set:
-            predicted_test_set.append(zip(untag(sent), self.Viterbi(untag(sent))))
-        return predicted_test_set
-
 def ComputeAccuracy(test_set, test_set_predicted):
     """ Using the gold standard tags in test_set, compute the sentence and tagging accuracy of test_set_predicted. """
     correct_sent_count = 0
@@ -144,8 +64,134 @@ def ComputeAccuracy(test_set, test_set_predicted):
     tagging_accuracy = ((float)(correct_tag_count) / (float)(num_words)) * 100.00
     print "Percent tagging accuracy in test set is %.2f%%." %tagging_accuracy
 
-def TreebankNoTraces():
-    return [[x for x in sent if x[1] != "-NONE-"] for sent in treebank.tagged_sents()]
+class TrigramHMM:
+    def __init__(self):
+        self.transitions = defaultdict(float)
+        self.emissions = defaultdict(float)
+        self.dictionary = defaultdict(lambda:list())
+
+    def GetUnigramCounts(self, training_set):
+    	unigram_counts = defaultdict(int)
+        for sent in training_set:
+            for word in sent:
+                unigram_counts[word[1]] += 1
+        return unigram_counts
+
+    def GetBigramCounts(self, training_set):
+    	bigram_counts = defaultdict(int)
+    	for sent in training_set:
+            prev = 0
+            for curr in xrange(1, len(sent)):
+                bigram = (sent[prev][1], sent[curr][1])
+                bigram_counts[bigram] += 1
+                prev += 1
+        return bigram_counts
+
+    def EstimateTransitions(self, training_set):
+        bigram_counts = self.GetBigramCounts(training_set)
+        for sent in training_set:
+            prev = 1
+            prev2 = 0
+            for curr in xrange(2, len(sent)):
+                trigram = (sent[prev2][1], sent[prev][1], sent[curr][1])
+                self.transitions[trigram] += 1
+                prev += 1
+                prev2 += 1
+        for trigram, freq in self.transitions.iteritems():
+            bigram = (trigram[0], trigram[1])
+            self.transitions[trigram] = (float)(freq) / (float)(bigram_counts[bigram])
+
+	"""
+    def EstimateTransitions(self, training_set):
+        bigram_counts = defaultdict(int)
+        unigram_counts = defaultdict(int)
+        for sent in training_set:
+            prev = 1
+            prev2 = 0
+            for curr in xrange(2, len(sent)):
+                trigram = (sent[prev2][1], sent[prev][1], sent[curr][1])
+                self.transitions[trigram] += 1
+                bigram = (sent[prev2][1], sent[prev][1])
+                bigram_counts[bigram] += 1
+                unigram = sent[prev2][1]
+                unigram_counts[unigram] += 1
+                prev += 1
+                prev2 += 1
+            bigram_counts[(sent[len(sent) - 2][1], sent[len(sent) - 1][1])] += 1
+            unigram_counts[sent[len(sent) - 2][1]] += 1
+            unigram_counts[sent[len(sent) - 1][1]] += 1
+        word_count = 0
+        for key in unigram_counts.keys():
+            word_count += unigram_counts[key]
+        for trigram, freq in self.transitions.iteritems():
+            bigram = (trigram[0], trigram[1])
+            self.transitions[trigram] = -1 #(0.4 * ((float)(freq) / (float)(bigram_counts[bigram]))) + (0.3 * ((float)(bigram_counts[(trigram[1], trigram[2])]) / (float)(unigram_counts[trigram[1]]))) + (0.3 * ((float)(unigram_counts[trigram[2]]) / (float)(word_count)))
+	"""
+
+    def EstimateEmissions(self, training_set):
+        unigram_counts = self.GetUnigramCounts(training_set)
+        for sent in training_set:
+            for bigram in sent:
+                self.emissions[bigram] += 1
+        for bigram, freq in self.emissions.iteritems():
+            self.emissions[bigram] = (float)(freq) / (float)(unigram_counts[bigram[1]])
+
+    def ComputeTagDictionary(self, training_set):
+        for sent in training_set:
+            for word in sent:
+                if word[1] not in self.dictionary[word[0]]:
+                    self.dictionary[word[0]].append(word[1])
+
+    def Train(self, training_set):
+    	self.EstimateTransitions(training_set)
+    	self.EstimateEmissions(training_set)
+    	self.ComputeTagDictionary(training_set)
+
+    def Viterbi(self, sent):
+        T = len(sent)
+        trellis = [{} for i in range(T)]
+        backptr = [{} for i in range(T)]
+
+        trellis[0][(start_token, start_token)] = 1.0
+        backptr[0][(start_token, start_token)] = None
+
+        for i in range(1, T):
+            prev_word = sent[i - 1]
+            prev_tags = self.dictionary[prev_word]
+            word = sent[i]
+            tags = self.dictionary[word]
+            assert(prev_tags)
+            assert(tags)
+            for prev_tag in prev_tags:
+            	for tag in tags:
+                	max_path_prob = None
+                	max_path_prev2_tag = None
+                	for (prev2_tag, prev1_tag), prev_path_prob in trellis[i - 1].iteritems():
+                		if prev_tag == prev1_tag:
+                  			path_prob = prev_path_prob * self.transitions[(prev2_tag, prev_tag, tag)] * self.emissions[(word, tag)]
+                   			if path_prob > max_path_prob and (max_path_prob or path_prob != float("-inf")):
+								max_path_prob = path_prob
+                       			max_path_prev2_tag = prev2_tag
+                	trellis[i][(prev_tag, tag)] = max_path_prob
+                	backptr[i][(prev_tag, tag)] = max_path_prev2_tag
+        #best_path_prob = trellis[T - 1][end_token]
+        best_path = self.Backtrace(sent, backptr)
+        return best_path
+
+    def Backtrace(self, sent, backptr, index = None, prev_tag = end_token, tag = end_token):
+        if index == 0:
+        	return [tag]
+        if index == None:
+        	index = len(sent) - 1
+        prev2_tag = backptr[index][(prev_tag, tag)]
+        prev_best_path = self.Backtrace(sent, backptr, index - 1, prev2_tag, prev_tag)
+        return prev_best_path + [tag]
+
+    def Test(self, test_set):
+        predicted_test_set = list()
+        for sent in test_set:
+            predicted_test_set.append(zip(untag(sent), self.Viterbi(untag(sent))))
+        return predicted_test_set
 
 def main():
     """
